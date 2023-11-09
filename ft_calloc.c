ad7c605d16d16d3536ce1e496336afd2aeabcf5f
Camille Truchot <ctruchot@bess-f4r2s12.clusters.42paris.fr>
2023-11-09 18:33:59 +0100

    v2bis

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ctruchot <ctruchot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:18:25 by ctruchot          #+#    #+#             */
-/*   Updated: 2023/11/09 18:18:46 by ctruchot         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:31:07 by ctruchot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void *calloc(size_t nmemb, size_t size)
 {
+	void *ptr;
 	
+	ptr = malloc (size * nmemb);
+	if (!ptr)
+		return (NULL);
+	// The memory is set to zero. 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	return (ptr)
 }
